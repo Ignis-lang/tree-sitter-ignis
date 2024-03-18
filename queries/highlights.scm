27f196b12ee55bdba1a353bf0d71a91f40cedf3b
Ignacio Perez <ignacio.perez2123@protonmail.com>
2024-03-18 17:47:26 -0300

    Added operator highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -8,7 +8,7 @@
 
 (null_literal) @constant
 
-["=" "<" "<=" ">" ">=" "!" "++" "--" "||" "&&" "/" "*" "+" "%" "&" "|" "=="] @operator
+["-" "=" "<" "<=" ">" ">=" "!" "++" "--" "||" "&&" "/" "*" "+" "%" "&" "|" "=="] @operator
 
 (expression (identifier) @variable.property)
 
